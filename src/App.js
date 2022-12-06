b8b76c2c1191a7fbeb701b2af12d4577896a6376
TroelsAgergaard <troelsagergaard@gmail.com>
2022-12-06 19:53:35 +0100

    Add a little ui sugar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Navigation from "./Components/Navigation";
 import Loginform from "./Components/Loginform";
 import MySchedule from "./Components/MySchedule";
 import { AuthContextProvider } from "./Context/Authcontext";
-import cat from "./Assets/Cat03.jpeg";
 
 function App() {
   return (
     <AuthContextProvider>
-      <div className="App">
-        <Navigation />
-        <img src={cat} />
+      <div className="flex flex-col items-center justify-center h-screen">
         <Routes>
-          <Route path="/login" element={<Loginform />} />
+          <Route path="/" element={<Loginform />} />
           <Route path="/myschedule" element={<MySchedule />} />
         </Routes>
       </div>
--- src/Components/MySchedule.js
+++ src/Components/MySchedule.js
@@ -1,15 +1,22 @@
 import { useContext } from "react";
 import { AuthContext } from "../Context/Authcontext";
 import { Link } from "react-router-dom";
-// import { AddToHomeScreen } from 'react-pwa-add-to-homescreen';
-
 
 const MySchedule = () => {
   let { isAuthticated } = useContext(AuthContext);
   return (
-    <div>
-      {/* {isAuthticated && <div>myschedule<AddToHomeScreen /></div>} */}
-      {!isAuthticated && <Link to="/login">Klik for at logge ind!</Link>}
+    <div class="flex flex-col text-center">
+      {isAuthticated && <h1 className="text-4xl">My Schedule</h1>}
+      {!isAuthticated && (
+        <>
+          <h3 className="text-1xl">
+            You need to be logged in to see your schedule!
+          </h3>
+          <Link className="mt-5 underline" to="/">
+            Take me to login
+          </Link>
+        </>
+      )}
     </div>
   );
 };
